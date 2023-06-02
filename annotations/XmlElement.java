00b500c31f25e9c6d355231e6b12afcdf70cc7ce
prowler47 <78755942+prowler47@users.noreply.github.com>
2023-06-02 14:07:46 +0300

    Create XmlElement.java

---
+++ annotations/XmlElement.java
@@ -0,0 +1,12 @@
+package dragunovskiy_sergey.xml_serializer.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface XmlElement {
+    String key() default "";
+}
